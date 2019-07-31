c2d156483c87b29dd0c003b15975be2b57889037
Marcos Roberto Martins de Oliveira <marcosbeto@gmail.com>
2019-07-31 18:08:28 -0300

    Handling API errors.31

--- usuarios_meviro/admin.py
+++ usuarios_meviro/admin.py
@@ -271,7 +271,7 @@ class PacotePorUsuarioAdmin(admin.ModelAdmin):
     	print(response.text)
     	extra_context = {'access_token': '123'}
     	url = reverse('admin:%s_%s_changelist' % ('usuarios_meviro', 'pacoteporusuario'))
-    	messages.success(request, content.decode("utf-8").json())
+    	messages.success(request, content.decode("utf-8"))
     	return HttpResponseRedirect(url)
     	# return super(PacotePorUsuarioAdmin, self).changelist_view(request, extra_context=extra_context)
 
