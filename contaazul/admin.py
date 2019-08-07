1197f122283ce145877596a64a03ba612d1ce7e5
Marcos Roberto Martins de Oliveira <marcosbeto@gmail.com>
2019-08-07 15:19:28 -0300

    Creating contaazul app.2

---
+++ contaazul/admin.py
@@ -0,0 +1,123 @@
+from django.contrib import admin
+from .models import Token
+from django import forms
+from django.contrib import admin
+import requests
+from django.urls import include, path
+from django.conf.urls import url
+from django.utils.html import format_html
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.template.response import TemplateResponse
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+import json, codecs
+from django.shortcuts import render
+from django.contrib import messages
+import base64
+
+
+class ActiveFilterForm(forms.Form):
+    code = forms.CharField()
+    state = forms.CharField()
+
+class TokenAdmin(admin.ModelAdmin):
+	search_fields = ['token']
+
+	advanced_search_form = ActiveFilterForm()
+	change_list_template = "admin/contaazul/token/change_list.html"
+	actions = ['sincronizar_pacotes_contaazul', 'acessar_auth_token']
+
+	def get_urls(self):
+		urls = super().get_urls()
+		my_urls = [
+			path('sincronizar_pacotes_contaazul/', self.admin_site.admin_view(self.sincronizar_pacotes_contaazul), name='sincronizar_pacotes_contaazul'),
+			path('acessar_auth_token/', self.admin_site.admin_view(self.acessar_auth_token), name='acessar_auth_token'),
+		]
+	    
+		return my_urls + urls
+
+	def get_changelist(self, request, **kwargs):
+
+	    from django.contrib.admin.views.main import ChangeList
+	    code = self.other_search_fields.get('code',None)
+	    # now we have the active_pp parameter that was passed in and can use it.
+
+	    class ActiveChangeList(ChangeList):
+	    	def get_query_set(self, *args, **kwargs):
+	    		now = datetime.datetime.now()
+	    		qs = super(ActiveChangeList, self).get_query_set(*args, **kwargs)
+	    		return qs.filter((Q(start_date=None) | Q(start_date__lte=now))
+					& (Q(end_date=None) | Q(end_date__gte=now)))
+
+	    if not code is None:
+	    	return ActiveChangeList
+
+	    return ChangeList
+
+	def lookup_allowed(self, lookup):
+		if lookup in self.advanced_search_form.fields.keys():
+			return True
+		return super(MyModelAdmin, self).lookup_allowed(lookup)
+
+	def sincronizar_pacotes_contaazul(self, request):
+		if request.method == 'GET':
+			client_id = 'pPIYG4rGDP11A0CHTeanFTSLeGiZNGuE'
+			state_code = 'orivem'
+			endpoint = 'https://api.contaazul.com/auth/authorize?redirect_uri={REDIRECT_URI}&client_id={CLIENT_ID}&scope=sales&state={STATE}'
+			url = endpoint.format(REDIRECT_URI='http://mevirospace.herokuapp.com/admin/usuarios_meviro/pacoteporusuario/', CLIENT_ID=client_id, STATE=state_code)
+		return HttpResponseRedirect(url)
+
+	def acessar_auth_token(self, request, code):
+		client_id = 'pPIYG4rGDP11A0CHTeanFTSLeGiZNGuE'
+		client_key = 'H3l6iIiNYgsYyjh6m5sWZ8WMoKL5rOBy'
+		to_encode = '{CLIENT_ID}:{CLIENT_KEY}'.format(CLIENT_ID=client_id, CLIENT_KEY=client_key)
+		encoded = base64.b64encode(to_encode.encode('ascii'))
+		headers={'Authorization': 'Basic %s' % encoded.decode("utf-8")}
+		post_data = {'grant_type': 'authorization_code', 'redirect_uri': 'http://mevirospace.herokuapp.com/admin/usuarios_meviro/pacoteporusuario/', 'code': code}
+		response = requests.request("POST", 'https://api.contaazul.com/oauth2/token/', params=post_data, headers=headers)
+		# requests.request("POST", url, headers=headers, params=querystring)
+		content = response.content
+		print("CONTEEEEEENT")
+		print(response.text)
+		extra_context = {'access_token': '123'}
+		url = reverse('admin:%s_%s_changelist' % ('usuarios_meviro', 'pacoteporusuario'))
+		messages.success(request, json.loads(content.decode("utf-8")))
+		return HttpResponseRedirect(url)
+		# return super(PacotePorUsuarioAdmin, self).changelist_view(request, extra_context=extra_context)
+
+	def changelist_view(self, request, extra_context=None):
+	    self.other_search_fields = {} 
+	    
+	    code = request.GET.get('code')
+	    access_token = request.GET.get('access_token')
+	    refresh_token = request.GET.get('refresh_token')
+
+	    asf = self.advanced_search_form
+	    extra_context = {'asf':asf}
+
+	    request.GET._mutable=True
+
+	    for key in asf.fields.keys():
+	        try:
+	            temp = request.GET.pop(key)
+	        except KeyError:
+	            pass 
+	        else:
+	            if temp!=['']: 
+	                self.other_search_fields[key] = temp 
+
+	    request.GET_mutable=False
+	    extra_context = {'code': code, 'access_token': access_token, 'refresh_token': refresh_token} 
+
+	    if (code):
+	    	self.acessar_auth_token(request, code)
+
+	    print("REQUEST:")
+	    print(request)
+
+	    return super(TokenAdmin, self).changelist_view(request, extra_context=extra_context)
+	    
+    # list_display = ('nome', 'descricao', 'data_', 'data_implantacao')
+
+admin.site.register(Token, TokenAdmin)
---
+++ contaazul/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Token(models.Model):
+	token = models.CharField(max_length=200, blank=True, null=True, verbose_name="Refresh Token")
+	refresh_token = models.CharField(max_length=200, blank=True, null=True, verbose_name="Refresh Token")
+	hora_atualizacao = models.TimeField(blank=True, null=True,)
+
+	def __str__(self):
+		return u'%s' % (self.refresh_token)
+
+	class Meta:
+		verbose_name = "Token"
+		verbose_name_plural = "Tokens"
