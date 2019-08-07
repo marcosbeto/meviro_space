43f0e6ef751769db41f20f838f8cfeac84867db9
Marcos Roberto Martins de Oliveira <marcosbeto@gmail.com>
2019-08-07 20:34:04 -0300

    Creating contaazul app.27

--- contaazul/admin.py
+++ contaazul/admin.py
@@ -77,18 +77,14 @@ class TokenAdmin(admin.ModelAdmin):
 
 		post_data = {'grant_type': 'refresh_token', 'refresh_token': current_refresh_token_str}
 		response = requests.request("POST", 'https://api.contaazul.com/oauth2/token/', params=post_data, headers=headers)
-		# requests.request("POST", url, headers=headers, params=querystring)
 		content = response.content
 		content_json = json.loads(content.decode("utf-8"))
 
 		access_token = content_json['access_token']
 		refresh_token = content_json['refresh_token']
 		Token.objects.filter(pk=1).update(token=access_token, refresh_token=refresh_token, hora_atualizacao=datetime.datetime.now())
-		
-		print("CONTEEEEEENT")
-		print(response.text)
 		url = reverse('admin:%s_%s_changelist' % ('contaazul', 'token'))
-		messages.success(request, current_refresh_token_str)
+		messages.success(request, 'Token atualizado: %s' % current_refresh_token_str)
 		return HttpResponseRedirect(url)
 
 	def requisitar_autenticacao_inicial(self, request):
