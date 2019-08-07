a21abc415a2a0c895f72b2a84b83b262197e3e19
Marcos Roberto Martins de Oliveira <marcosbeto@gmail.com>
2019-08-07 20:32:21 -0300

    Creating contaazul app.26

--- contaazul/models.py
+++ contaazul/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Token(models.Model):
-	token = models.CharField(max_length=200, blank=True, null=True, verbose_name="Refresh Token")
+	token = models.CharField(max_length=200, blank=True, null=True, verbose_name="Access Token")
 	refresh_token = models.CharField(max_length=200, blank=True, null=True, verbose_name="Refresh Token")
 	hora_atualizacao = models.TimeField(blank=True, null=True,)
 
 	def __str__(self):
-		return u'%s' % (self.refresh_token)
+		return u'%s' % (self.token)
 
 	class Meta:
 		verbose_name = "Token"
