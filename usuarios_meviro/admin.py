f217170b39abf5f5b13853024a17ff1a78901080
Marcos Roberto Martins de Oliveira <marcosbeto@gmail.com>
2019-08-10 11:06:04 -0300

    Creating contaazul app.92

--- usuarios_meviro/admin.py
+++ usuarios_meviro/admin.py
@@ -174,7 +174,7 @@ class PacotePorUsuarioAdmin(admin.ModelAdmin):
     	for id_pacote_por_usuario in array_id_pacotes_por_usuario:
     		usuario_espaco = UsuarioEspaco.objects.get(id_contaazul=id_contaazul)
     		pacote = Pacote.objects.get(id_contaazul=id_pacote_por_usuario)
-    		pacote_por_usuario_database = PacotePorUsuario.objects.filter(usuario__in=usuario_espaco, pacote__in=pacote, id_venda_contaazul=id_venda)
+    		pacote_por_usuario_database = PacotePorUsuario.objects.filter(usuario=usuario_espaco, pacote=pacote, id_venda_contaazul=id_venda)
     		if not pacote_por_usuario_database:
     			pacote_por_usuario = PacotePorUsuario(usuario=usuario_espaco,pacote=pacote,ativo=False,data_ativacao=None,data_encerramento=None,id_venda_contaazul=id_venda)
     			pacote_por_usuario.save()
