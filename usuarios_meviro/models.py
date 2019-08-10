fe1fe3c80d363fd0e083752c9bda1c0bf6e2965c
Marcos Roberto Martins de Oliveira <marcosbeto@gmail.com>
2019-08-10 11:12:48 -0300

    Creating contaazul app.96

--- usuarios_meviro/models.py
+++ usuarios_meviro/models.py
@@ -60,8 +60,8 @@ class PacotePorUsuario(models.Model):
 	usuario = models.ForeignKey(UsuarioEspaco, on_delete=models.DO_NOTHING, verbose_name="Nome do Usuário")
 	pacote = models.ForeignKey(Pacote, on_delete=models.DO_NOTHING, verbose_name="Pacote")
 	ativo = models.BooleanField(default=False, verbose_name="Este plano está ativo?")
-	data_ativacao = models.DateField(verbose_name="Data de Ativação")
-	data_encerramento = models.DateField(verbose_name="Data de Encerramento")
+	data_ativacao = models.DateField(blank=True, null=True, verbose_name="Data de Ativação")
+	data_encerramento = models.DateField(blank=True, null=True, verbose_name="Data de Encerramento")
 	id_venda_contaazul = models.CharField(max_length=100, blank=True, null=True, verbose_name="ID Venda ContaAzul")
 
 	class Meta:
