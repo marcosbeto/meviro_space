062e3e18d2aa7861e6b81c6d5c9191c2304d229e
Marcos Roberto Martins de Oliveira <marcosbeto@gmail.com>
2019-08-15 16:36:53 -0300

    Refactoring how token is saved.45

--- administrativo/admin.py
+++ administrativo/admin.py
@@ -32,29 +32,26 @@ class PacoteAdmin(admin.ModelAdmin):
     
 	def save_model(self, request, obj, form, change):
 		#Todo: tratar excecoes
-		try:
-			token = self.interfaceToken.atualizar_token()
-			headers = self.interfaceToken.set_authorization_header('bearer', token)
-		
-			post_data = {"name": form.data['nome'], "value": form.data['valor_venda'], "cost": form.data['valor_custo'], "code": form.data['codigo']}
-		
-			if form.data['id_contaazul']: #atualizando pacote
-				response_content_json = self.interfaceToken.request_contaazul('save_service', "https://api.contaazul.com/v1/services/%s" % form.data['id_contaazul'], None, json.dumps(post_data), headers)
-				messages.success(request, response_content_json)
-			else:
-				response_content_json = self.interfaceToken.request_contaazul('update_service', "https://api.contaazul.com/v1/services", None, json.dumps(post_data), headers)
+		token = self.interfaceToken.atualizar_token()
+		headers = self.interfaceToken.set_authorization_header('bearer', token)
+	
+		post_data = {"name": form.data['nome'], "value": form.data['valor_venda'], "cost": form.data['valor_custo'], "code": form.data['codigo']}
+	
+		if form.data['id_contaazul']: #atualizando pacote
+			response_content_json = self.interfaceToken.request_contaazul('save_service', "https://api.contaazul.com/v1/services/%s" % form.data['id_contaazul'], None, json.dumps(post_data), headers)
+			messages.success(request, response_content_json)
+		else:
+			response_content_json = self.interfaceToken.request_contaazul('update_service', "https://api.contaazul.com/v1/services", None, json.dumps(post_data), headers)
 
-				_mutable = form.data._mutable
-				form.data._mutable = True
-				form.data['id_contaazul'] = response_content_json['id']
-				obj.id_contaazul = response_content_json['id']
-				form.data._mutable = _mutable
-				#TODO: melhorar modelo de mensagens de resposta e excecoes
-				messages.success(request, response_content_json)
-				super(PacoteAdmin, self).save_model(request, obj, form, change)
-		except:
-			return messages.error(request, "Não foi possível completar sua requisição.")
-
+			_mutable = form.data._mutable
+			form.data._mutable = True
+			form.data['id_contaazul'] = response_content_json['id']
+			obj.id_contaazul = response_content_json['id']
+			form.data._mutable = _mutable
+			#TODO: melhorar modelo de mensagens de resposta e excecoes
+			messages.success(request, response_content_json)
+			
+		super(PacoteAdmin, self).save_model(request, obj, form, change)
 		
 
 class RegraAdmin(admin.ModelAdmin):
